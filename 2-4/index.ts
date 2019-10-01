bd7b9ed8600a543ac35c0eb343e6726baa679d3f
yamatatsu <the.kinnikuman@gmail.com>
2019-10-01 22:50:45 +0900

    2-4

--- 2-4/index.ts
+++ 2-4/index.ts
@@ -2,16 +2,20 @@ console.log("exec in 2-4 dir !!!");
 
 const { open, copy, stdout } = Deno;
 
-const fileNames = ["a.txt", "b.txt", "c.txt"];
+// const fileNames = ["a.txt", "b.txt", "c.txt"];
 
 // 早すぎてどのみち標準出力が混ざることはなかった
 
 // fileNames.forEach(f => open(f).then(src => copy(stdout, src)));
 
-fileNames.reduce(
-  (promise: Promise<number>, f) =>
-    promise
-      .then(() => open(f))
-      .then(src => copy(stdout, src).finally(() => src.close())),
-  Promise.resolve(0)
+// fileNames.reduce(
+//   (promise: Promise<number>, f) =>
+//     promise
+//       .then(() => open(f))
+//       .then(src => copy(stdout, src).finally(() => src.close())),
+//   Promise.resolve(0)
+// );
+
+Promise.all([open("denoland.html", "w+"), fetch("https://deno.land")]).then(
+  ([f, res]) => copy(f, res.body).finally(() => f.close())
 );
